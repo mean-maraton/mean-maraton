1972e92afdde8a58b63810b01fdd260885de1200
bulgar1 <boraexpress@yahoo.com>
2016-04-28 10:26:39 -0700

    angular test

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,11 +1,47 @@
-(function () {
-  'use strict';
+//.DS_Store(function () {
+//'use strict';
 
-  angular
-    .module('core')
-    .controller('HomeController', HomeController);
+//  angular.module('core').controller('HomeController', HomeController);
 
-  function HomeController() {
-    var vm = this;
+//  function HomeController() {
+//    var vm = this;
+//  }
+//}());   //figering uot the andular 2.0 is not cool the docs suk!!!!!!!!!!!!!!!!!!
+"use strict";
+
+angular.module("core").controller("HomeController", ["$scope", "Authentication",
+  function($scope, Authentication) {
+    // aut context
+    $scope.authentication = Authentication;
+
+    $scope.alerts = [{
+        icon: "giyphicon-user",
+        colour: "btn-succes",
+        total: "20,408",
+        description: "total customers"
+      }
+
+      {
+        icon: "giyphicon-user",
+        colour: "btn-succes",
+        total: "20,408",
+        description: "total customers"
+      } {
+        icon: "giyphicon-user",
+        colour: "btn-succes",
+        total: "20,408",
+        description: "total customers"
+      } {
+        icon: "giyphicon-user",
+        colour: "btn-succes",
+        total: "20,408",
+        description: "new customers"
+      } {
+        icon: "giyphicon-user",
+        colour: "btn-succes",
+        total: "20,408",
+        description: "total inventorie"
+      }
+    ];
   }
-}());
+]);
