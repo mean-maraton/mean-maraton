a30ea381189f9bb7a518ba6774b84a5bbe321d5d
bulgar1 <boraexpress@yahoo.com>
2016-05-05 08:59:35 -0700

    ar.cl.ro

--- modules/articles/client/config/articles.client.routes.js
+++ modules/articles/client/config/articles.client.routes.js
@@ -1,6 +1,6 @@
-(function () {
+(function() {
   'use strict';
-
+  ////////////////////////////////////////////////////////////
   angular
     .module('articles.routes')
     .config(routeConfig);
